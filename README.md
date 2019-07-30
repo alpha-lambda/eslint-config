df69abf5696e620eabe14d20b09ae2ebfefa4a71
Hekar Khani <Hekar.Khani@D2L.com>
2019-07-30 15:05:21 -0400

    initial version

---
+++ README.md
@@ -0,0 +1,34 @@
+# eslint-config
+
+Common Alpha Lambda eslint config.
+
+## Installation
+
+```
+npm install @alpha-lambda/eslint-config
+```
+
+### Usage
+
+Simply specify the extends property in the .eslintrc file as shown below. Note: omit the "/eslint-config" when specifying the module, since eslint assumes it.
+
+## Default Config
+Specify the extends property in the .eslintrc.json file:
+
+{
+  "extends": "@alpha-lambda"
+}
+
+## Code Style
+This repository is configured with EditorConfig rules and contributions should make use of them.
+
+## License
+The MIT License (MIT)
+
+Copyright (c) 2019 Hekar Khani
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,96 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:import/recommended'
+    ],
+    plugins: [
+        'import'
+    ],
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+    rules: {
+        'array-bracket-spacing': [
+            'error',
+            'never'
+        ],
+        'arrow-body-style': 0,
+        'arrow-parens': [
+            'error',
+            'as-needed'
+        ],
+        'comma-dangle': [
+            'error',
+            'always-multiline'
+        ],
+        'comma-spacing': [
+            'error',
+            { before: false, after: true }
+        ],
+        'implicit-arrow-linebreak': 0,
+        'import/newline-after-import': [
+            'error'
+        ],
+        'import/no-cycle': [
+            'error'
+        ],
+        'import/no-unresolved': [
+            'error',
+            { commonjs: true, caseSensitive: true }
+        ],
+        'import/no-extraneous-dependencies': [
+            'error',
+            { optionalDependencies: false, devDependencies: ['test/**'] }
+        ],
+        'import/order': [
+            'error',
+            { groups: ['builtin', 'external', 'internal'] }
+        ],
+        'indent': [
+            'error',
+            4,
+            { SwitchCase: 1 }
+        ],
+        'keyword-spacing': [
+            'error'
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'newline-per-chained-call': 0,
+        'no-multi-spaces': [
+            'error'
+        ],
+        'no-shadow': [
+            'error',
+            { builtinGlobals: true, hoist: 'all' }
+        ],
+        'no-underscore-dangle': 0,
+        'no-unused-expressions': 0,
+        'object-curly-spacing': [
+            'error',
+            'always'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'semi-spacing': [
+            'error',
+            { before: false, after: true }
+        ],
+        'spaced-comment': [
+            'error',
+            'always'
+        ]
+    }
+}
